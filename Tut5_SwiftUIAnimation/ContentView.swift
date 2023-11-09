2ea0a519b4695ec179b3fb0b6eb6535dd4d9a09a
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-09 23:30:32 +0600

    Initial Commit

---
+++ Tut5_SwiftUIAnimation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tut5_SwiftUIAnimation
+//
+//  Created by ZEUS on 9/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tut5_SwiftUIAnimation/Tut5_SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tut5_SwiftUIAnimationApp.swift
+//  Tut5_SwiftUIAnimation
+//
+//  Created by ZEUS on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Tut5_SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
