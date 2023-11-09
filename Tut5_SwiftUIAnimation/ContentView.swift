a26dccb06747233105bcf52525cbb601b9580947
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-09 23:41:22 +0600

    -> Added Circle and a heart inside a zStack -> Added TapGesture for zStack, tapping on the Circle the heart icon transforms to big/ small with background color changed.

--- Tut5_SwiftUIAnimation/ContentView.swift
+++ Tut5_SwiftUIAnimation/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var circleColorChanged = false
+    @State private var heartColorChanged = false
+    @State private var heartSizeChanged = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Circle()
+                .frame(width: 200, height: 200)
+                .foregroundColor(circleColorChanged ? Color(.systemGray) : .red)
+            
+            Image(systemName: "heart.fill")
+                .foregroundColor(heartColorChanged ? .red : .white)
+                .font(.system(size: 100))
+                .scaleEffect(heartSizeChanged ? 1.0 : 0.5)
         }
-        .padding()
+        .onTapGesture {
+            circleColorChanged.toggle()
+            heartColorChanged.toggle()
+            heartSizeChanged.toggle()
+        }
+
     }
 }
 
