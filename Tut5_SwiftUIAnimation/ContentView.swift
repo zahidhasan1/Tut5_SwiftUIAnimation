dbefc059fa4e658548da6547789e7a3548a029f2
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-10 02:26:37 +0600

    Added Loader like animation using CircleView and rotationEffect.

--- Tut5_SwiftUIAnimation/ContentView.swift
+++ Tut5_SwiftUIAnimation/ContentView.swift
@@ -12,36 +12,11 @@ struct ContentView: View {
     @State private var circleColorChanged = false
     @State private var heartColorChanged = false
     @State private var heartSizeChanged = false
+    @State private var isLoading = false
     var body: some View {
-        ZStack {
-            Circle()
-                .frame(width: 200, height: 200)
-                .foregroundColor(circleColorChanged ? Color(.systemGray) : .red)
-
-            
-            Image(systemName: "heart.fill")
-                .foregroundColor(heartColorChanged ? .red : .white)
-                .font(.system(size: 100))
-                .scaleEffect(heartSizeChanged ? 1.0 : 0.5)
-        }
-        //.animation(.default, value: circleColorChanged)
-       // .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3) ,value: heartSizeChanged)
-
-        //.animation(.default, value: circleColorChanged)
-
-        .onTapGesture {
-//            withAnimation(.default) {
-//                circleColorChanged.toggle()
-//                heartColorChanged.toggle()
-//                heartSizeChanged.toggle()
-//            }
-            
-            withAnimation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3)){
-                circleColorChanged.toggle()
-                heartSizeChanged.toggle()
-            }
-            heartColorChanged.toggle()
-            
+        VStack {
+            HeartFillAnimation(circleColorChanged: $circleColorChanged, heartColorChanged: $heartColorChanged, heartSizeChanged: $heartSizeChanged)
+            CircleLoaderAnimation(isLoading: $isLoading)
         }
 
     }
@@ -50,5 +25,61 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct HeartFillAnimation: View {
+    
+    @Binding var circleColorChanged: Bool
+    @Binding var heartColorChanged: Bool
+    @Binding var heartSizeChanged: Bool
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .frame(width: 200, height: 200)
+                .foregroundColor(circleColorChanged ? Color(.systemGray) : .red)
+            
+            
+            Image(systemName: "heart.fill")
+                .foregroundColor(heartColorChanged ? .red : .white)
+                .font(.system(size: 100))
+                .scaleEffect(heartSizeChanged ? 1.0 : 0.5)
+        }
+        //.animation(.default, value: circleColorChanged)
+        // .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3) ,value: heartSizeChanged)
+        
+        //.animation(.default, value: circleColorChanged)
+        
+        .onTapGesture {
+            //            withAnimation(.default) {
+            //                circleColorChanged.toggle()
+            //                heartColorChanged.toggle()
+            //                heartSizeChanged.toggle()
+            //            }
+            
+            withAnimation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3)){
+                circleColorChanged.toggle()
+                heartSizeChanged.toggle()
+            }
+            heartColorChanged.toggle()
+            
+        }
+    }
+}
+
+struct CircleLoaderAnimation: View{
+    @Binding var isLoading: Bool
+    var body: some View{
+        Circle()
+            .trim(from: 0, to: 0.7)
+            .stroke(Color.green, lineWidth: 15)
+            .frame(width: 150, height: 150)
+            .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+            //.animation(.default.repeatForever(autoreverses: false), value: isLoading)
+            .animation(.linear(duration: 0.5).repeatForever(autoreverses: false), value: isLoading)
+            .onAppear(){
+                isLoading = true
+            }
     }
 }
