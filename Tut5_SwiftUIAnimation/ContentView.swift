4c5227046f840d68826ef046dd2ca82d6f9ad506
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-09 23:50:30 +0600

    Added Animation Effect

--- Tut5_SwiftUIAnimation/ContentView.swift
+++ Tut5_SwiftUIAnimation/ContentView.swift
@@ -17,12 +17,18 @@ struct ContentView: View {
             Circle()
                 .frame(width: 200, height: 200)
                 .foregroundColor(circleColorChanged ? Color(.systemGray) : .red)
+
             
             Image(systemName: "heart.fill")
                 .foregroundColor(heartColorChanged ? .red : .white)
                 .font(.system(size: 100))
                 .scaleEffect(heartSizeChanged ? 1.0 : 0.5)
         }
+        //.animation(.default, value: circleColorChanged)
+        .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3) ,value: heartSizeChanged)
+
+        .animation(.default, value: circleColorChanged)
+
         .onTapGesture {
             circleColorChanged.toggle()
             heartColorChanged.toggle()
